077e77b70eb84a27f4f98970fd85a4cb2f32a690
bun913 <nagasima.romance+@gmail.com>
2024-04-09 06:28:12 +0900

    feat: initial commit

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { proxyStore } from '../app/proxyStore';
-import Popup from './Popup';
+// import Popup from './Popup';
 
 proxyStore.ready().then(() => {
   createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
       <Provider store={proxyStore}>
-        <Popup />
+        <div>へいへい！</div>
       </Provider>
     </React.StrictMode>
   );
